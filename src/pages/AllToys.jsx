ab2887732ceecef1789343028dd1f8d6f1a9cb39
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-05-19 18:27:01 +0600

    Add all toys table

--- src/pages/AllToys.jsx
+++ src/pages/AllToys.jsx
@@ -1,8 +1,79 @@
+import { Link } from "react-router-dom";
+import Container from "../components/Shared/Container";
+import { toys } from "../../data/toys";
 const AllToys = () => {
   return (
-    <div>
-      <h1>All Toys</h1>
-    </div>
+    <Container className="my-24">
+      <h1 className="text-center text-4xl font-bold mb-8">All Toys</h1>
+      {/* Table */}
+      <div className="overflow-x-auto my-8 w-full mx-auto ">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Toy Name</th>
+              <th>Sub Category</th>
+              <th>Price</th>
+              <th>Available</th>
+              <th>Seller Name</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {toys.slice(0, 20).map((toy) => (
+              <tr key={toy.id}>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src="/tailwind-css-component-profile-2@56w.png"
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{toy.toyName}</div>
+                      {/* <div className="text-sm opacity-50">United States</div> */}
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {toy.subCategory}
+                  <br />
+                  <span className="badge badge-ghost badge-sm">Toys</span>
+                </td>
+                <td>${toy.price}</td>
+                <th>
+                  <span className="btn btn-ghost btn-xs">{toy.quantity}</span>
+                </th>{" "}
+                <th>
+                  <span className="btn btn-ghost btn-xs">{toy.seller}</span>
+                </th>
+                <th>
+                  <Link to={`/toys/${toy.link}`}>
+                    <button className="btn btn-outline btn-xs">
+                      View Details
+                    </button>
+                  </Link>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+          {/* foot */}
+          {/* <tfoot>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </tfoot> */}
+        </table>
+      </div>
+    </Container>
   );
 };
 
